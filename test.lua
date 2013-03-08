76ced4732211f788293c2c1b688138a303bc51b1
Clement Farabet <clement.farabet@gmail.com>
2013-03-08 00:32:50 -0500

    Paramaters are passed to the algorithm.

--- test.lua
+++ test.lua
@@ -6,9 +6,13 @@ require 'rosenbrock'
 
 -- Test LBFGS:
 x = torch.zeros(10)
-lbfgs(rosenbrock, x, {report = function(state)
-   print(state)
-end})
+lbfgs(rosenbrock, x, {
+   linesearch = 'LBFGS_LINESEARCH_BACKTRACKING_WOLFE',
+   max_iterations = 100,
+   report = function(state)
+      print(state)
+   end
+})
 
 -- Solution
 print('Solution:')
